5a91e03d5981ca816b22b4778b3a07ae2573e24f
Daniil Rybakov <61760429+DaniilRyb@users.noreply.github.com>
2022-03-13 22:34:00 +0300

    some fix v1

--- lab03/lab03.js
+++ lab03/lab03.js
@@ -1,13 +1,14 @@
 let p = 4 // размер окна
 let w = [0, 0, 0, 0, 0] // веса
-let a = 0
-let b = 4
-let N = 20
+const a = 0
+const b = 4
+const N = 20
 let step = (b - a) / N
 let vectorValue = []
-let x = []
+let VectorX = []
 let x_t
-let norma = 0.3
+const norma = 0.3
+
 
 for (let t = a; t < b + step; t += step) {
   x_t = (0.5 * Math.cos(0.5 * t)) - Math.sin(t)
@@ -15,65 +16,62 @@ for (let t = a; t < b + step; t += step) {
 
 }
 for (let i = 0; i < 4; i++) {
-  x.push(vectorValue[16 + i])
+  VectorX.push(vectorValue[16 + i])
 }
 
 for (let t = b + step; t <= (2 * b) - a + step; t += step) {
   x_t = (0.5 * Math.cos(0.5 * t)) - Math.sin(t)
-  x.push(x_t)
-
+  VectorX.push(x_t)
 }
 
-/*
-console.log("Вектор реальных значений ф-ии")
-for (let i = 0; i < x.length; i++) {
-  console.log(i, x[i])
-}
-*/
 
-const era = w => {
-  let epsilon, VectorP = [], sum = 0
-  for (let n = p; n < x.length; n++) {
-    let summa = 0, x_n = 0, VectorPrognoz = [];
-    for (let k = 1; k <= p; k++) {
-      x_n += w[k] * x[n - p + k - 1]
-      x_n += w[0]
-    }
-    console.log( n + " x_n = " + x_n,'x[n] = ' + x[n] )
-
-    let beta = x[n] - x_n
-    //console.log("beta = " + beta)
-    w[0] += norma * beta
-    w[1] += norma * beta * x[n - 3]
-    w[2] += norma * beta * x[n - 2]
-    w[3] += norma * beta * x[n - 1]
-    w[4] += norma * beta * x[n]
-    sum = summa += beta * beta
-    VectorP = VectorPrognoz.push(x_n)
+const eraSlidingWindow = w => {
+  let x_n = []
+  for (let i = 0; i < 4; i++) {
+    x_n.push(vectorValue[16 + i])
   }
-  //console.log("сумма эпохи = " + sum)
-
-  epsilon = Math.pow(sum, 0.5)
+  var sum = 0
+  for (let n = p; n < VectorX.length; n++) {
+    let xn_ = 0
+    for (let k = 1; k < p; k++) {
+      xn_ += w[k] * VectorX[n - p + k - 1]
+    }
+    //xn_ += w[0]
+    x_n.push(xn_)
+    let beta = VectorX[n] - x_n[n]
+    w[1] += norma * beta * VectorX[n - 3]
+    w[2] += norma * beta * VectorX[n - 2]
+    w[3] += norma * beta * VectorX[n - 1]
+    w[4] += norma * beta * VectorX[n]
+    sum += (beta * beta)
+  }
 
   return {
     weight: w,
-    epsilon: epsilon,
-    predictedX: VectorP
+    epsilon: Math.pow(sum, 0.5),
+    x: x_n
   }
 }
 
-let eraSlidingWindow = {}
-let k = 0
-const timeSeriesForecast = w => {
-  for (let i = 0; i < 1; i++) {
-    eraSlidingWindow = era(w)
-    //console.log("текущая ошибка " + i + " = " +  eraSlidingWindow.epsilon)
-    w = eraSlidingWindow.weight
-    /*if (eraSlidingWindow.epsilon < 0.5) {
-      break
-    }*/
+
+let prognoz = w => {
+  let k = 0
+  let result = eraSlidingWindow(w)
+  for (let i = 0; i < 1000; i++) {
+    result = eraSlidingWindow(w)
+    w = result.weight
+    result = eraSlidingWindow(w)
+    if (result.epsilon < 0.16) break
+    console.log(i + " " + result.epsilon)
+    k++
+  }
+
+  return {
+    res: result.x,
+    k: k
   }
 }
-timeSeriesForecast(w)
 
-
+let res = prognoz(w)
+console.log(res)
+console.log(VectorX)
