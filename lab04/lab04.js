a27a793df65ad56037b59686d3383a68a92c9f78
Daniil Rybakov <61760429+DaniilRyb@users.noreply.github.com>
2022-03-20 00:36:07 +0300

    update lab04 RBF network

--- lab04/lab04.js
+++ lab04/lab04.js
@@ -1,9 +1,9 @@
-/* Исследование нейронных сетей с радиальными базисными функциями (RBF) на примере моделирования булевых выражений */
+/* Исследование нейронных сетей с радиальными базисными функциями (RBF)
+на примере моделирования булевых выражений */
+
 const learning_rate = 0.3
 const fi0 = 1
 let tTeacher = [1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0] // значение t на наборах переменных
-
-let V = [0, 0, 0, 0, 0, 0, 0, 0]
 
 // значения всех возможных наборов аргументов
 let X1 = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]
@@ -19,16 +19,6 @@ let Cji = [[1, 1, 0, 0],
     [0, 1, 1, 1],
     [1, 1, 1, 1]]
 
-let Y1 = [0, 0, 1, 1]
-let Y2 = [0, 1, 0, 1]
-let Y3 = [1, 1, 1, 0]
-let Y4 = [0, 0, 1, 1]
-
-let x_1 = [0, 0, 1, 0]
-let x_2 = [0, 1, 1, 0]
-let x_3 = [1, 0, 1, 1]
-let x_4 = [1, 1, 0, 1]
-
 
 const porogovayaFunc = net => net >= 0 ? 1 : 0
 
@@ -40,9 +30,8 @@ const hyperbolicTangentOut = x =>
 
 const fiCalculation = (X, Cji, j) => {
     let sum = 0
-    let C = Cji[j]
     for (let i = 0; i < 4; i++) {
-        sum += ((X[i] - C[i]) * (X[i] - C[i]))
+        sum += ((X[i] - Cji[j][i]) * (X[i] - Cji[j][i]))
     }
     return Math.exp(-sum)
 }
@@ -57,26 +46,30 @@ const calculateNet = (Cji, V, X) => {
 }
 
 const hammingDistance = (teacher, y) => { // считаем расстояние Хэмминга
-    let E = 0
+    let Error = 0
     for (let i = 0; i < teacher.length; i++) {
         if (teacher[i] !== y[i]) {
-            E++
+            Error++
         }
     }
-    return E
+    return Error
 }
 
 const stepsMinVectorNeuralNetwork = (X1, X2, X3, X4, coeff, combination) => {
     let y = 0,
         outputVector = [],
         vectorT = []
+
     for (let i = 0; i < combination.length; i++) {
-        let X = [X1[combination[i]], X2[combination[i]], X3[combination[i]], X4[combination[i]]]
+        let X = [X1[combination[i]],
+            X2[combination[i]],
+            X3[combination[i]],
+            X4[combination[i]]]
         let net = calculateNet(Cji, coeff, X)
-        let out = hyperbolicTangentOut(net)
-        y = logisticFunc(out)
+        //let out = hyperbolicTangentOut(net)
+        y = porogovayaFunc(net)
         outputVector.push(y)
-        let t = tTeacher[i]
+        let t = tTeacher[combination[i]]
         vectorT.push(t)
         let beta = t - y
         if (beta !== 0) {
@@ -85,23 +78,19 @@ const stepsMinVectorNeuralNetwork = (X1, X2, X3, X4, coeff, combination) => {
             for (let i = 1; i < coeff.length; i++) {
                 coeff[i] += learning_rate * beta * fiCalculation(X, Cji, i - 1)
             }
+
             /*
-                        V[0] += learning_rate * beta * fi0
-                        for (let i = 1; i < V.length; i++) {
-                            V[i] += learning_rate * beta * fiCalculation(X, Cji, i - 1) *
+                        coeff[0] += learning_rate * beta * fi0 *
+                            (1 / (2 * Math.pow(((Math.exp(net) + Math.exp(-net)) / 2), 2)))
+                        for (let i = 1; i < coeff.length; i++) {
+                            coeff[i] += learning_rate * beta * fiCalculation(X, Cji, i - 1) *
                                 (1 / (2 * Math.pow(((Math.exp(net) + Math.exp(-net)) / 2), 2)))
                         }
             */
 
         } else {
-            coeff[0] = coeff[0]
-            coeff[1] = coeff[1]
-            coeff[2] = coeff[2]
-            coeff[3] = coeff[3]
-            coeff[4] = coeff[4]
-            coeff[5] = coeff[5]
-            coeff[6] = coeff[6]
-            coeff[7] = coeff[7]
+            for (let i = 0; i < coeff.length; i++)
+                coeff[i] = coeff[i]
         }
     }
 
@@ -142,16 +131,22 @@ const learningNetworkMinVector = V => {
 
 const eraNeuralNetwork = (coefficient, combination) => {
 
-    let currentEra = {}, k = 0;
+    let currentEra = {},
+        k = 0;
 
     while (currentEra.Error !== 0) {
         currentEra = stepsMinVectorNeuralNetwork(X1, X2, X3, X4, coefficient, combination)
-        //console.log(currentEra, 'эпоха ', k)
+        console.log(currentEra, 'эпоха ', k)
         k++
         if (k > 150) break
         coefficient = currentEra.coefficients
     }
-    return [coefficient, combination, k]
+
+    return {
+        coefficient: coefficient,
+        combination: combination,
+        Epoch_count: k
+    }
 }
 
 function* combinationN(array, n) {
@@ -169,7 +164,7 @@ function* combinationN(array, n) {
     }
 }
 
-function* combination_ordered(array) {
+function* combinations(array) {
     for (let i = 1; i <= array.length; i++) {
         yield* combinationN(array, i);
     }
@@ -177,19 +172,18 @@ function* combination_ordered(array) {
 
 let index = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-let generator = combination_ordered(index)
+let combinations_generator = combinations(index)
 
 let eraSearchVector = {}
-
-for (let combination of generator) {
-    let coeff = [0, 0, 0, 0, 0, 0, 0, 0]
-    eraSearchVector = eraNeuralNetwork(coeff, combination)
-    let minVector = learningNetworkMinVector(eraSearchVector[0])
+for (let combinations of combinations_generator) {
+    let V_coeff = [0, 0, 0, 0, 0, 0, 0, 0]
+    eraSearchVector = eraNeuralNetwork(V_coeff, combinations)
+    let minVector = learningNetworkMinVector(eraSearchVector.coefficient)
     if (minVector.Error !== 0) continue
     else break
 }
 
-//eraNeuralNetwork(V, X1, X2, X3, X4)
-
-console.log("найденные веса: " + eraSearchVector[0] + "\n",
-    "индексы минимальных векторов: " + eraSearchVector[1] + "\n", "количество эпох: " + eraSearchVector[2])
+console.log("найденные синаптические веса: "
+    + eraSearchVector.coefficient + "\n",
+    "индексы минимальных векторов: " + eraSearchVector.combination + "\n",
+    "количество эпох: " + eraSearchVector.Epoch_count)
