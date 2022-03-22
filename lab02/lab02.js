6314bcf59b27dc76987d84643973f23969790380
Daniil Rybakov <61760429+DaniilRyb@users.noreply.github.com>
2022-03-22 14:20:35 +0300

    lab03 finished

--- lab02/lab02.js
+++ lab02/lab02.js
@@ -1,13 +1,13 @@
 /* Применение однослойной нейронной сети с линейной функцией активации для прогнозирования временных рядов */
 
-const slidingWindow = (a, b, p, N, norma) => {
+const slidingWindow = (a, b, p, N, learning_rate, countEpoch) => {
 
     let w = []
     for (let i = 0; i < p + 1; i++) {
         w.push(0)
     }
 
-    const step = (b - a) / N
+    let step = (b - a) / N
     let vectorValue = []
     let VectorX = []
     let x_t
@@ -18,7 +18,7 @@ const slidingWindow = (a, b, p, N, norma) => {
     }
 
     for (let i = 0; i < p; i++) {
-        VectorX.push(vectorValue[16 + i])
+        VectorX.push(vectorValue[vectorValue.length - 1 - p + i])
     }
 
     for (let t = b + step; t <= (2 * b) - a + step; t += step) {
@@ -26,13 +26,15 @@ const slidingWindow = (a, b, p, N, norma) => {
         VectorX.push(x_t)
     }
 
-
     const stepEraSlidingWindow = w => {
+
         let x_n = []
         for (let i = 0; i < p; i++) {
-            x_n.push(vectorValue[16 + i])
+            x_n.push(vectorValue[vectorValue.length - 1 - p + i])
         }
+
         var sum = 0
+
         for (let n = p; n < VectorX.length; n++) {
             let xn_ = 0
             for (let k = 1; k < p; k++) {
@@ -41,7 +43,7 @@ const slidingWindow = (a, b, p, N, norma) => {
             x_n.push(xn_)
             let beta = VectorX[n] - x_n[n]
             for (let i = 1; i < p + 1; i++) {
-                w[i] += norma * beta * VectorX[n - p + i]
+                w[i] += learning_rate * beta * VectorX[n - p + i]
             }
             sum += (beta * beta)
         }
@@ -53,29 +55,53 @@ const slidingWindow = (a, b, p, N, norma) => {
         }
     }
 
-    let eraSlidingWindow = w => {
-        let k = 0, error = 10
-        let result = {}
-        for (let i = 0; i < 5000; i++) {
-            result = stepEraSlidingWindow(w)
+    let methodSlidingWindow = weight => {
+
+        let k = 0,
+            error = 100,
+            result = {}
+
+        for (let i = 0; i < countEpoch; i++) {
+            result = stepEraSlidingWindow(weight)
             let minError = result.epsilon
-            k++
             if (minError < error) error = minError
             else break
-            console.log(result.epsilon)
-            w = result.weight
+            k++
+            //console.log(result.epsilon)
+            weight = result.weight
         }
 
         return {
-            res: result.x,
-            k: k,
-            w: w
+            predictedValues: result.x,
+            epochCount: k,
+            weight: weight,
+            minError: error
         }
     }
-    let result = eraSlidingWindow(w)
-    console.log(result.w)
-    console.log("VectorX", VectorX, "\nresult", result.res)
-    console.log(result.k)
+
+    let result = methodSlidingWindow(w)
+
+    /*console.log("weight ", result.weight,
+        "\n обучающая выборка ", VectorX,
+        "\n спрогнозированные значения", result.predictedValues,
+        "\n кол-во эпох", result.epochCount,
+        "\n минимальная ошибка при заданном \'p'", result.minError)*/
+    console.log(result.minError)
 }
 
-slidingWindow(0, 4, 4, 20, 0.5)
+let pVector = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+let a = 0,
+    b = 4,
+    N = 20
+
+for (let i = 0; i < pVector.length; i++) {
+    slidingWindow(a, b, pVector[i], N, 0.5, 10000)
+}
+
+for (let i = 0; i < 1; i += 0.01) {
+    slidingWindow(a, b, 6, N, i, 10000)
+}
+
+for (let countEpoch = 10; countEpoch < 500; countEpoch += 10) {
+    slidingWindow(a, b, 6, N, 0.5, countEpoch)
+}
