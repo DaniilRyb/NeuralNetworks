52863f69da7efa62cd43b07dec2da2c68a82ee84
Daniil Rybakov <61760429+DaniilRyb@users.noreply.github.com>
2022-04-10 14:58:06 +0300

    added fixes

--- RK2/RK2.js
+++ RK2/RK2.js
@@ -1,13 +1,7 @@
-/*
-Архитектура нейронной сети 1 – 1 – 3
-xInputVector  = (1, –3)
-yOutputVector = (1, 1, 1)
-*/
-
-const activationFunc = net => 1 / (1 + Math.exp(-net))
+const activationFunc = net => (1 / (1 + Math.exp(-net)))
 
 const derivativeActivationFunc = net =>
-    activationFunc(net) * (1 - activationFunc(net))
+    (activationFunc(net) * (1 - activationFunc(net)))
 
 const errorEpsilonCalculation = (M, t_j, yj_k) => {
     let sum = 0
@@ -19,27 +13,27 @@ const errorEpsilonCalculation = (M, t_j, yj_k) => {
 
 const methodBackPropagation = (weights, learning_rate) => {
 
+    // const t_j = [0.1, 0.1, 0.1] // целевой выход
     const t_j = [1, 1, 1] // целевой выход
     const x1 = -3, x2 = 1 // 2 входа
+    let w1_11, w1_01,
+        w2_11, w2_12, w2_13,
+        w2_01, w2_02, w2_03
 
-    let w11_1, w01_1,
-        w11_2, w12_2, w13_2,
-        w01_2, w02_2, w03_2
+    w1_11 = weights[0]
+    w1_01 = weights[1]
+    w2_11 = weights[2]
+    w2_12 = weights[3]
+    w2_13 = weights[4]
+    w2_01 = weights[5]
+    w2_02 = weights[6]
+    w2_03 = weights[7]
 
-    w11_1 = weights[0]
-    w01_1 = weights[1]
-    w11_2 = weights[2]
-    w01_2 = weights[3]
-    w12_2 = weights[4]
-    w02_2 = weights[5]
-    w13_2 = weights[6]
-    w03_2 = weights[7]
-
-    let net1_1 = w11_1 * x1 + w01_1 * x2,
+    let net1_1 = w1_01 * x2 + w1_11 * x1,
         x1_2 = activationFunc(net1_1),
-        net1_2 = w11_2 * x1_2 + w01_2 * 1,
-        net2_2 = w12_2 * x1_2 + w02_2 * 1,
-        net3_2 = w13_2 * x1_2 + w03_2 * 1
+        net1_2 = w2_01 * 1 + w2_11 * x1_2,
+        net2_2 = w2_02 * 1 + w2_12 * x1_2,
+        net3_2 = w2_03 * 1 + w2_13 * x1_2
 
     let y1 = activationFunc(net1_2),
         y2 = activationFunc(net2_2),
@@ -53,7 +47,7 @@ const methodBackPropagation = (weights, learning_rate) => {
 
         delta3 = (t_j[2] - yj_k[2]) * derivativeActivationFunc(net3_2),
 
-        delta1_1 = ((w11_2 * delta1) + (w12_2 * delta2) + (w13_2 * delta3))
+        delta1_1 = ((w2_11 * delta1) + (w2_12 * delta2) + (w2_13 * delta3))
             * derivativeActivationFunc(net1_1)
 
     let M = 3,
@@ -61,19 +55,18 @@ const methodBackPropagation = (weights, learning_rate) => {
 
 
     // коррекция весов 1 слоя
-    w11_1 += learning_rate * x1 * delta1_1
-    w01_1 += learning_rate * delta1_1
+    w1_11 += learning_rate * x1 * delta1_1
+    w1_01 += learning_rate * delta1_1
 
     // коррекция весов 2 слоя
-    w11_2 += learning_rate * x1_2 * delta1
-    w01_2 += learning_rate * delta1
-    w12_2 += learning_rate * x1_2 * delta2
-    w02_2 += learning_rate * delta2
-    w13_2 += learning_rate * x1_2 * delta3
-    w03_2 += learning_rate * delta3
+    w2_11 += learning_rate * x1_2 * delta1
+    w2_01 += learning_rate * delta1
+    w2_12 += learning_rate * x1_2 * delta2
+    w2_02 += learning_rate * delta2
+    w2_13 += learning_rate * x1_2 * delta3
+    w2_03 += learning_rate * delta3
 
-    weights = [w11_1, w01_1, w11_2, w01_2,
-        w12_2, w02_2, w13_2, w03_2]
+    weights = [w1_11, w1_01, w2_11, w2_12, w2_13, w2_01, w2_02, w2_03]
 
     console.log(
         "x1(1)=" + x1 + "\n" +
@@ -92,14 +85,14 @@ const methodBackPropagation = (weights, learning_rate) => {
         "delta2[3]=" + delta3 + "\n" +
         "delta1[1]=" + delta1_1 + "\n" +
 
-        "W1[0,1]=" + w01_1 + "\n" +
-        "W1[1,1]=" + w11_1 + "\n" +
-        "W2[0,1]=" + w01_2 + "\n" +
-        "W2[1,1]=" + w11_2 + "\n" +
-        "W2[0,2]=" + w02_2 + "\n" +
-        "W2[1,2]=" + w12_2 + "\n" +
-        "W3[0,3]=" + w03_2 + "\n" +
-        "W3[1,3]=" + w13_2
+        "W1[0,1]=" + w1_01 + "\n" +
+        "W1[1,1]=" + w1_11 + "\n" +
+        "W2[0,1]=" + w2_01 + "\n" +
+        "W2[1,1]=" + w2_11 + "\n" +
+        "W2[0,2]=" + w2_02 + "\n" +
+        "W2[1,2]=" + w2_12 + "\n" +
+        "W2[0,3]=" + w2_03 + "\n" +
+        "W2[1,3]=" + w2_13 + "\n"
     )
 
     return {
@@ -110,7 +103,7 @@ const methodBackPropagation = (weights, learning_rate) => {
 }
 
 const epochLearningNeuralNetwork = weights => {
-    let counterEpoch = 0,
+    var counterEpoch = 0,
         valueError,
         currentBackPropagationEpoch = {}
 
@@ -133,5 +126,5 @@ const epochLearningNeuralNetwork = weights => {
 }
 
 /* w11_1, w01_1, w11_2, w12_2, w13_2, w01_2, w02_2, w03_2 */
-let weights = [-0.6, 0.5, -0.1, -0.6, 0.9, 0.5, 0.5, 0.5]
+var weights = [-0.6, 0.5, -0.1, -0.6, 0.9, 0.5, 0.5, 0.5]
 epochLearningNeuralNetwork(weights)
